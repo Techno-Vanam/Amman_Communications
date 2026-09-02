8582df5b910ae5caf5d969a88e316e7a9ad94a4b
Bavithran100 <bavithrannatarajan@gmail.com>
2026-09-02 09:52:40 +0530

    Final changes commited

--- apps/api/prisma/migrations/20260831040014_add_customer_profile_fields/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Customer" ADD COLUMN     "aadhaarNumber" TEXT,
-ADD COLUMN     "altPhone" TEXT,
-ADD COLUMN     "dob" TEXT,
-ADD COLUMN     "emergencyContact" TEXT,
-ADD COLUMN     "isProfileCompleted" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "occupation" TEXT,
-ADD COLUMN     "panNumber" TEXT;
